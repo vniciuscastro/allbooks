09093feaa0e4f076b71768a7a062b8d36177f773
Vinicius Castro Candido de Aquino <vinylogan@hotmail.com>
2025-08-11 17:07:10 -0300

    altera o server

--- server.js
+++ server.js
@@ -363,5 +363,5 @@ https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt')
 }, server).listen(8000, () => {
-   console.log("Acesse essa API em https://localhost:8000")
+   console.log("API disponível em https://localhost:8000")
 })
